b951e7b90bf81a3017cb3b06fc6a8e72491bea45
M1guelzinN <antonio.jobs1397@gmail.com>
2022-05-28 23:11:30 -0300

    copy-button

--- itens/index.js
+++ itens/index.js
@@ -1,4 +1,6 @@
-function password(){
+var alerta = document.querySelector('#aviso')
+
+function password(){  
     let carac = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%&{}[]><?-_.()"
     let passwordLength = 12;
     let password = ""
@@ -8,4 +10,14 @@ function password(){
         password += carac.substring(RandomN, RandomN + 1)
     }
     document.querySelector('#senha').value = password
+    alerta.innerHTML = `Senha<br><strong>${password}</strong><br> foi copiada`
+}
+
+function copy(){
+    let copiar = document.querySelector("#senha").value
+    navigator.clipboard.writeText(copiar)
+
+    alerta.classList.toggle('active')
+    setTimeout(() => {
+    alerta.classList.toggle('active')}, 2000)
 }
