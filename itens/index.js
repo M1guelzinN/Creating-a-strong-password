a39a5498113188ed3e98b78d5ba14568f28be1f6
M1guelzinN <antonio.jobs1397@gmail.com>
2022-06-26 16:55:51 -0300

    cleancode

--- itens/index.js
+++ itens/index.js
@@ -1,24 +1,25 @@
-const alerta = document.querySelector('#aviso')
+const alerta = document.querySelector('#aviso');
 
-function password(){  
-    let carac = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%&{}[]><?-_.()"
-    let passwordLength = 12
-    let password = ""
+function CreateAPassword() {
+  const carac = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%&{}[]><?-_.()';
+  const passwordLength = 12;
+  let password = '';
 
-    for(let r = 0; r < passwordLength; r++){
-        let RandomN = Math.floor(Math.random() * carac.length)
-        password += carac.substring(RandomN, RandomN + 1)
-    }
-    
-    document.querySelector('#senha').value = password
-    alerta.innerHTML = `Senha<br><strong>${password}</strong><br> foi copiada`
+  for (let r = 0; r < passwordLength; r += 1) {
+    const RandomN = Math.floor(Math.random() * carac.length);
+    password += carac.substring(RandomN, RandomN + 1);
+  }
+
+  document.querySelector('#senha').value = password;
+  alerta.innerHTML = `Senha<br><strong>${password}</strong><br> foi copiada`;
 }
 
-function copy(){
-    const copiar = document.querySelector("#senha").value
-        navigator.clipboard.writeText(copiar)
+function copy() {
+  const copiar = document.querySelector('#senha').value;
+  navigator.clipboard.writeText(copiar);
 
-    alerta.classList.toggle('active')
-        setTimeout(() => {
-            alerta.classList.toggle('active')}, 2000)
+  alerta.classList.toggle('active');
+  setTimeout(() => {
+    alerta.classList.toggle('active');
+  }, 2000);
 }
